905939e7afe30c9fecd53e2cf7559cc135ba4cd7
skngatsu <155278683+skngatsu@users.noreply.github.com>
2024-01-02 00:11:38 -0300

    segundoCommit

--- SQL/bd.sql
+++ SQL/bd.sql
@@ -53,4 +53,6 @@ ALTER TABLE sistema_votacion.votacion ADD INDEX(rut);
 INSERT INTO `regiones` VALUES (1,'Region 1'),(2,'Region 2'),(3,'Region 3');
 INSERT INTO `comunas` VALUES (1,'Comuna 1',1),(2,'Comuna 2',2),(3,'Comuna 3',3),(4,'Comuna 4',2);
 INSERT INTO `candidato` VALUES (8,'Candidato 8',4),(7,'Candidato 7',4),(1,'Candidato 1',1),(2,'Candidato 2',1),(3,'Candidato 3',2),(4,'Candidato 3',2),(5,'Candidato 5',3),(6,'Candidato 6',3);
+INSERT INTO `votacion` VALUES (null,'prueba1','prueba','','eduardo.patricio@gmail.com',1,1,1,1,1,0,0);
+
 
